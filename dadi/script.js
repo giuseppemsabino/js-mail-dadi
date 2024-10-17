a3f8134ac9cda22d7751115c58f29131068de0d6
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-10-17 17:57:54 +0200

    die complete

--- dadi/script.js
+++ dadi/script.js
@@ -5,9 +5,9 @@
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 
 // Che ci sia un array da qualche parte?
-//no usero il .random 
+//no usero il .random
 // Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-//
+//un if o un operatore terziario
 
 // Consigli del giorno:
 // 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
@@ -22,30 +22,27 @@ const startGame = prompt('inserisci "start" se vuoi comimciare');
 
 //* se l'input e "start" faccio girare un for su un metodo random 2 volte per avere i due numeri(una per il giocatore uno per la macchina)
 
-if(startGame.includes('start')){
+if (startGame.includes("start")) {
+  let userNumber = 0;
+  let pcNumber = 0;
 
-    let userNumber = 0;
-    let pcNumber = 0;
-
- for(let i = 0; i < 1; i++){
-  
-    userNumber =  Math.floor(Math.random(1) * 6)  + 1;
+  for (let i = 0; i < 1; i++) {
+    userNumber = Math.floor(Math.random(1) * 6) + 1;
     pcNumber = Math.floor(Math.random(1) * 6) + 1;
-    console.log(' il tuo numero è : '+ userNumber, '\n il numero della macchina è : ' + pcNumber);
- }
-//*dopo con un if o un operatore terziario verificare quale dei due numeri sia maggiore e assegno una vincitore
+    console.log(
+      " il tuo numero è : " + userNumber,
+      "\n il numero della macchina è : " + pcNumber
+    );
+  }
+  //*dopo con un if o un operatore terziario verificare quale dei due numeri sia maggiore e assegno una vincitore
 
-if(userNumber === pcNumber){
-    alert('è un pareggio')
-}
-
-
-
- 
-
-
-
-
-
-
+  if (userNumber > pcNumber) {
+    alert("hai vinto, il tuo numero e maggiore");
+  } else if (userNumber < pcNumber) {
+    alert("vince la macchina il tuo numeroe minore");
+  } else {
+    alert("è un pareggio");
+  }
+} else {
+  alert("Hai paura del successo?");
 }
